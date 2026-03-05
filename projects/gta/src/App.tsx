a09f90f3b78936bce5549a50fb336290e0516bc6
ami-inn <amipk2001@gmail.com>
2026-03-05 16:03:49 +0530

    update: init the proj with tailwind

---
+++ projects/gta/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const App = () => {
+  return <div className="text-indigo-300">h1 </div>;
+};
+
+export default App;
